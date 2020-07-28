3a15f6b2551bfd046e6bbc1caa3340af07c8843d
Almerindo Paixão <almerindo_paixao@live.com>
2020-07-28 14:33:01 -0300

    update aula 2

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -5,6 +5,13 @@ export default function CadastroCategoria() {
   return (
     <div className="main">
       <h1>Cadastro de categoria</h1>
+      <form>
+        <label htmlFor="nome">
+          Nome da Categoria:
+          <input type="text" />
+        </label>
+        <button type="submit">Cadastrar</button>
+      </form>
       <Link to="/">Ir para home</Link>
     </div>
   );
